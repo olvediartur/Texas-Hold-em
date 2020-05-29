9eb5b312db6360416a78f9b824e94032a5a2018f
Nagy Balázs <balage86620@gmail.com>
2020-05-29 21:32:17 +0200

    First Code
    A gitignort majd át kell rágni, hogy mi az amit ne toljunk át egymásnak.
    Ebben van egy csomó szemét is.
    Bocsi

---
+++ TexasHoldem/src/hu/ak_akademia/texasholdem/control/Game.java
@@ -0,0 +1,12 @@
+/**
+ * Ez egy minta class, hogy ne legyen üres a package
+ */
+package hu.ak_akademia.texasholdem.control;
+
+/**
+ * @author bnagy
+ *
+ */
+public class Game {
+
+}
---
+++ TexasHoldem/src/hu/ak_akademia/texasholdem/view/Main.java
@@ -0,0 +1,29 @@
+/**
+ * Ez egy minta class, hogy ne legyen üres a package
+ * A program belépési pontja.
+ */
+package hu.ak_akademia.texasholdem.view;
+
+/**
+ * @author bnagy
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new Main().run();
+
+	}
+
+	/**
+	 * Áttérés példány szintre
+	 */
+	private void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
