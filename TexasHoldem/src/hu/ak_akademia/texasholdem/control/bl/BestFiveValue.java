80231feb36a7813fb16f0a59ec90bf31ed348800
Nagy Balázs <balage86620@gmail.com>
2020-06-24 19:16:13 +0200

    BestFive vol.7

--- TexasHoldem/src/hu/ak_akademia/texasholdem/control/bl/BestFiveValue.java
+++ TexasHoldem/src/hu/ak_akademia/texasholdem/control/bl/BestFiveValue.java
@@ -13,8 +13,9 @@ import hu.ak_akademia.texasholdem.model.deck.Figure;
  *
  */
 public enum BestFiveValue {
-	ROYAL_FLUSH(1, "royalflush"), STRAIGHT_FLUSH(2, ""), FOUR_OF_KIND(3, ""), FULL_HOUSE(4, ""), FLUSH(5, ""),
-	STRAIGHT(6, ""), THREE_OF_KIND(7, ""), TWO_PAIR(8, ""), PAIR(9, ""), HIGH_CARD(10, "");
+	ROYAL_FLUSH(1, "royalflush"), STRAIGHT_FLUSH(2, "straightflush"), FOUR_OF_KIND(3, "poker"),
+	FULL_HOUSE(4, "full"), FLUSH(5, "flush"), STRAIGHT(6, "straight"), THREE_OF_KIND(7, "drill"),
+	TWO_PAIR(8, "twopairs"), PAIR(9, "pair"), HIGH_CARD(10, "highcard");
 
 	private int value; // mező
 	private String textual;
@@ -63,8 +64,87 @@ public enum BestFiveValue {
 		if (isPair(cards)) {
 			return BestFiveValue.PAIR;
 		}
+		return BestFiveValue.HIGH_CARD; // enum beli elemmel tér vissza (ha nem 1-9-ig akkor 10)
+	}
 
-		return HIGH_CARD; // enum beli elemmel tér vissza (ha nem 1-9-ig akkor 10)
+	/**
+	 * @param cards
+	 * @return igaz, ha mind az 5 kártya egyszínű
+	 */
+	private static boolean isFlush(Card[] cards) { // szín megegyezik
+		CardColor cc = cards[0].getColor();
+		for (Card c : cards) {
+			if (!cc.equals(c.getColor())) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @param cards
+	 * @return igaz, az öt kártya figurájának az értéke sorban követi egymást
+	 */
+	private static boolean isStraight(Card[] cards) {
+		// 5 egymás után következő kártya
+		Card first = cards[0];
+		for(int i = 1; i < cards.length;i++) {
+			if(i + first.getFigure().getValue() != cards[i].getFigure().getValue()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * @param cards
+	 * @return igaz, ha egyszínű és sor
+	 */
+	private static boolean isStraightFlush(Card[] cards) {
+		if(!isFlush(cards)) {
+			return false;
+		}
+		if(!isStraight(cards)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param cards
+	 * @return igaz, ha színsor és Ász a legnagyobb
+	 */
+	private static boolean isRoyalFlush(Card[] cards) {
+		if(!isStraightFlush(cards)) {
+			return false;
+		}
+		if (!cards[0].getFigure().equals(Figure.ACE)) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param cards
+	 * @return
+	 */
+	private static boolean isFourOfKind(Card[] cards) {
+		Card first = cards[0];
+		Card second = cards[1];
+		if(first.getFigure().getValue() == second.getFigure().getValue()) {
+			for(int i = 2; i <= 3; i++) {
+				if(cards[i].getFigure().getValue() != first.getFigure().getValue()) {
+					return false;
+				}
+			}
+		} else {
+			for(int i = 2; i <= 4; i++) {
+				if(cards[i].getFigure().getValue() != second.getFigure().getValue()) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 
 	/**
@@ -120,72 +200,93 @@ public enum BestFiveValue {
 //		return false;
 //	}
 
+	/**
+	 * @param cards
+	 * @return akkor igaz, ha van két pár és egy drill
+	 */
 	private static boolean isFullHouse(Card[] cards) {
 		// 1 drill és egy pár
-		return false;
-	}
-
-	private static boolean isFlush(Card[] cards) { // szín megegyezik
-		CardColor cc = cards[0].getColor();
-		for (Card c : cards) {
-			if (!cc.equals(c.getColor())) {
-				return false;
-			}
-			;
+		if(!isThreeOfKind(cards)) {
+			return false;
+		}
+		if(!isTwoPair(cards)) {
+			return false;
 		}
 		return true;
 	}
 
-	private static boolean isStraight(Card[] cards) {
-		// 5 egymás után következő kártya
-		return false;
-	}
-
-	private static boolean ThreeOfKind(Card[] cards) {
+	/**
+	 * @param cards
+	 * @return
+	 * Három féle képpen lehet
+	 * AAAXY
+	 * XAAAY
+	 * XYAAA
+	 * Akkor igaz, ha valamelyik fenáll
+	 */
+	private static boolean isThreeOfKind(Card[] cards) {
 		// drill
+		if(cards[0].getFigure().getValue() == cards[1].getFigure().getValue()
+				&& cards[1].getFigure().getValue() == cards[2].getFigure().getValue()
+			) {
+			//AAAXY
+			return true;
+		}
+		if(cards[1].getFigure().getValue() == cards[2].getFigure().getValue()
+				&& cards[2].getFigure().getValue() == cards[3].getFigure().getValue()
+			) {
+			//XAAAY
+			return true;
+		}
+		if(cards[2].getFigure().getValue() == cards[3].getFigure().getValue()
+				&& cards[3].getFigure().getValue() == cards[4].getFigure().getValue()
+			) {
+			//XYAAA
+			return true;
+		}
 		return false;
 	}
 
-	private static boolean TwoPair(Card[] cards) {
-		// két pár
-		return false;
-	}
-
-	private static boolean Pair(Card[] cards) {
-		// egy pár
-		return false;
-	}
-
-	private static boolean isRoyalFlush(Card[] cards) {
-		// egyszínű-e?
-		CardColor cc = cards[0].getColor();
-		for (Card c : cards) {
-			if (!cc.equals(c.getColor())) {
-				return false;
+	/**
+	 * @param cards
+	 * @return
+	 * Három féle képpen lehet két pár a kézben.
+	 * AABBX, AAXBB, XAABB
+	 * Akkor igaz, ha ezek valamelyike fennáll
+	 */
+	private static boolean isTwoPair(Card[] cards) {
+		if(cards[0].getFigure().getValue() == cards[1].getFigure().getValue()) {
+			if(cards[2].getFigure().getValue() == cards[3].getFigure().getValue()) {
+				//AABBX
+				return true;
 			}
-			;
+			if(cards[3].getFigure().getValue() == cards[4].getFigure().getValue()) {
+				//AAXBB
+				return true;
+			}
 		}
-		// ász-e az első ?
-		if (!cards[0].getFigure().equals(Figure.ACE)) {
-			return false;
+		if(cards[1].getFigure().getValue() == cards[2].getFigure().getValue()
+				&& cards[3].getFigure().getValue() == cards[4].getFigure().getValue()
+			) {
+			//XAABB
+			return true;
 		}
-		// Király-e a második
-		if (!cards[1].getFigure().equals(Figure.KING)) {
-			return false;
+		return false;
+	}
+
+	/**
+	 * @param cards
+	 * @return
+	 * Akkor igaz, ha két egymást követő kártya értéke egyenlő
+	 */
+	private static boolean isPair(Card[] cards) {
+		// egy pár
+		for(int i = 0; i < cards.length - 1;i++) {
+			if(cards[i].getFigure().getValue() == cards[i + 1].getFigure().getValue()) {
+				return true;
+			}
 		}
-		// dáma e a harmadik?
-		if (!cards[2].getFigure().equals(Figure.QUEEN)) {
-			return false;
-		}
-		// jack e a 4.?
-		if (!cards[3].getFigure().equals(Figure.QUEEN)) {
-			return false;
-		}
-		// 10-es e az 5.?
-		if (!cards[4].getFigure().equals(Figure.QUEEN)) {
-			return false;
-		}
-		return true;
+		return false;
 	}
 
 }
---
+++ TexasHoldem/src/test/TestBestFive.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package test;
+
+import hu.ak_akademia.texasholdem.model.deck.Card;
+
+/**
+ * @author bnagy
+ *
+ */
+public class TestBestFive {
+	
+	public static void main(String[] args) {
+		new TestBestFive().run();
+	}
+
+	private void run() {
+		int n = 5;
+		Card[] cards = new Card[n];
+		for(int i = 0; i < n; i++) {
+			
+		}
+		
+	}
+}
