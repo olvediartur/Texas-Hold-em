11693f13cf864a44f3ff18cdd5788b5d4d794695
karatechopmonkey <karatechopmonkey@gmail.com>
2020-07-01 22:10:05 +0200

    PayerInGameController class

---
+++ TexasHoldem/src/hu/ak_akademia/texasholdem/control/db/PlayerInGameController.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package hu.ak_akademia.texasholdem.control.db;
+
+import hu.ak_akademia.texasholdem.control.bl.BestFive;
+import hu.ak_akademia.texasholdem.model.db.PlayerInGameEntity;
+
+/**
+ * @author bnagy
+ *
+ */
+public class PlayerInGameController extends AbstractController<PlayerInGameEntity> {
+
+	public PlayerInGameController() {
+		super();
+		dao = new PlayerInGameDao();
+	}
+
+	@Override
+	public void setSelected(String[] dataFromUi) {
+		selected = new PlayerInGameEntity();
+		selected.setPokerUserId(Integer.parseInt(dataFromUi[0]));
+		selected.setGameId(Integer.parseInt(dataFromUi[0]));
+		selected.setBestCombination(BestFive.getBestFive(dataFromUi[0]));
+	}
+	
+	
+}
