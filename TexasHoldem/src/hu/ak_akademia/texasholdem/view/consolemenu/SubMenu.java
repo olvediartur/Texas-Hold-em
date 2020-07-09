5af37e87ae9117b7a11fe4d8f1d8f3ac4da6628f
Nagy Balázs <balage86620@gmail.com>
2020-07-09 10:19:19 +0200

    Menüpontok számozása

--- TexasHoldem/src/hu/ak_akademia/texasholdem/view/consolemenu/SubMenu.java
+++ TexasHoldem/src/hu/ak_akademia/texasholdem/view/consolemenu/SubMenu.java
@@ -24,18 +24,4 @@ public class SubMenu extends Menu {
 	public void select() {
 	}
 	
-	/**
-	 *
-	 */
-	@Override
-	public String show() {
-		StringBuilder sb = new StringBuilder();
-		sb.append( "[" + getTitle() + "]\n");
-		for(int i = 0; i < getOptions().size(); i++) {
-			sb.append(getIndex() + "." + getOptions().get(i).toString());
-			sb.append("\n");
-		}
-		return sb.toString();
-	}
-	
 }
