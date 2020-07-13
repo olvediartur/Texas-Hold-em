187f26a792ff822e5cf97a3faf2afa59dfc3f854
Nagy Balázs <balage86620@gmail.com>
2020-07-13 19:47:13 +0200

    TestingDataWizard progress

---
+++ TexasHoldem/src/test/TestDataWizard.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author bnagy
+ *
+ */
+public class TestDataWizard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new TestDataWizard().run();
+
+	}
+	
+	private void run() {
+		createTestUsers();
+		createTestGames();
+		
+	}
+
+	private void createTestUsers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void createTestGames() {
+		// TODO Auto-generated method stub
+		
+	}
+}
